30088437ebc2c0d205555e1e52bbbb6dd75ff1a3
sebinson <sebinson@163.com>
2015-03-16 16:22:44 +0800

    项目开发组织框架搭建

---
+++ sebinson-sample-soa-dubbo/src/main/java/net/sebinson/sample/soa/App.java
@@ -0,0 +1,13 @@
+package net.sebinson.sample.soa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ sebinson-sample-web-commons/src/main/java/net/sebinson/sample/web/App.java
@@ -0,0 +1,13 @@
+package net.sebinson.sample.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
