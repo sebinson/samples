c04e366d636b8c1b6654177c686196bd98f9c011
sebinson <sebinson@163.com>
2015-03-26 17:26:46 +0800

    xxx

--- sebinson-framework-message/src/main/java/net/sample/framework/message/transport/log/RemotingLog.java
+++ sebinson-framework-message/src/main/java/net/sample/framework/message/transport/log/RemotingLog.java
@@ -1,40 +1,28 @@
 package net.sample.framework.message.transport.log;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class RemotingLog
-{
-    protected static final Logger log = Logger.getLogger("pushlog");
+public class RemotingLog {
+    protected static final Logger log = LoggerFactory.getLogger("PUSH_LOG");
 
-    public static void debug(Object message)
-    {
-        if (log.isDebugEnabled())
-        {
+    public static void debug(String message) {
+        if (log.isDebugEnabled()) {
             log.debug(message);
         }
     }
 
-    public static void info(Object message)
-    {
-        if (log.isInfoEnabled())
-        {
+    public static void info(String message) {
+        if (log.isInfoEnabled()) {
             log.info(message);
         }
     }
 
-    public static void warn(Object message)
-    {
+    public static void warn(String message) {
         log.warn(message);
     }
 
-    public static void error(Object message, Throwable e)
-    {
+    public static void error(String message, Throwable e) {
         log.error(message, e);
     }
-
-    public static boolean getLevel(int level)
-    {
-        return log.getLoggerRepository().isDisabled(level);
-    }
-
 }
