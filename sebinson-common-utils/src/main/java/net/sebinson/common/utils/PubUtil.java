7a43521c651ddc84203f66edaf3e1ab689419822
sebinson <sebinson@163.com>
2015-03-26 15:37:29 +0800

    ccc

--- sebinson-common-utils/src/main/java/net/sebinson/common/utils/PubUtil.java
+++ sebinson-common-utils/src/main/java/net/sebinson/common/utils/PubUtil.java
@@ -1,7 +1,6 @@
 package net.sebinson.common.utils;
 
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
