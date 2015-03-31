808b07cef75d9cc261f43588645303108264c8e7
sebinson <sebinson@163.com>
2015-03-31 15:02:55 +0800

    xxx

--- sebinson-sample-soa-dubbo/sebinson-sample-soa-dubbo-consumer/src/main/java/net/sebinson/sample/soa/dubbo/consumer/Consumer.java
+++ sebinson-sample-soa-dubbo/sebinson-sample-soa-dubbo-consumer/src/main/java/net/sebinson/sample/soa/dubbo/consumer/Consumer.java
@@ -6,15 +6,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Consumer {
 
-    @SuppressWarnings("resource")
     public static void main(String[] args) throws Exception {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                new String[] { "classpath:spring-dubbo-consumer.xml" });
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "classpath:spring-dubbo-consumer.xml" });
         context.start();
 
         DemoService demoService = (DemoService) context.getBean("demoService"); // 获取远程服务代理
         String hello = demoService.sayHello("world"); // 执行远程方法
-        
+
         System.out.println(hello); // 显示调用结果
     }
 }
--- sebinson-sample-soa-dubbo/sebinson-sample-soa-dubbo-provider/src/main/java/net/sebinson/sample/soa/dubbo/provider/Provider.java
+++ sebinson-sample-soa-dubbo/sebinson-sample-soa-dubbo-provider/src/main/java/net/sebinson/sample/soa/dubbo/provider/Provider.java
@@ -3,10 +3,9 @@ package net.sebinson.sample.soa.dubbo.provider;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Provider {
-    @SuppressWarnings("resource")
+
     public static void main(String[] args) throws Exception {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-                new String[] { "classpath:spring-dubbo-provider.xml" });
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[] { "classpath:spring-dubbo-provider.xml" });
         context.start();
 
         System.in.read(); // 按任意键退出
