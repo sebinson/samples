d32b4f8c09a27839d4abb1302efeb4499692a239
sebinson <sebinson@163.com>
2015-04-07 11:43:53 +0800

    xxx

--- sebinson-framework-message/src/main/java/net/sebinson/framework/message/transport/protocol/Header.java
+++ sebinson-framework-message/src/main/java/net/sebinson/framework/message/transport/protocol/Header.java
@@ -15,40 +15,53 @@ import net.sebinson.common.utils.UUIDUtil;
  */
 public class Header implements Serializable {
 
-
     private static final long serialVersionUID = 4642888275245098817L;
 
     /**
-     * <b>required<br/>
-     * 0 request,1 response
+     * [Required] Request type [0 request,1 response]
      */
-    private int code = 0;
+    private int               code             = 0;
 
     /**
-     * <b>required<br/>
-     * response type[ 0 synchronize, 1 asynchronous, 2 no-response]
+     * [Required] Response type [0 synchronous, 1 asynchronous, 2 no-response]
      */
-    private int rpc = 1;
+    private int               rpc              = 1;
 
     /**
-     * <b>required<br/>
-     * Serial number
+     * [Required] Serial number
      */
-    private String serial = UUIDUtil.getUUID();
-    
-    
-    private String itype = "";// required, interface code,
+    private String            serial           = UUIDUtil.getUUID();
 
     /**
-     * [required] request identifier, the response is returned directly. such as
-     * the transponder MAC address, mobile phone token, socket client, the
-     * default server sends to the server ip
+     * [Required] Interface type
      */
-    private String add = HostUtil.getHostAddress();
-    private String ctime = TimeUtil.getCurrentDateTime("yyyyMMddHHmmssSSS");// [必选]此报文创建时间yyyyMMddHHmmssSSS",
-    private String sign = "SAMPLE";// 签名"
-    private String result = "";// [选填,主要用于应答] 0(处理成功),非0为其它异常编码"
-    private String msg = "";// [选填]描述（转发器初始化）"
+    private String            itype            = "";
+
+    /**
+     * [Required] Request identifier, such as the transporter mac address,
+     * mobile phone token, server ip.
+     */
+    private String            add              = HostUtil.getHostAddress();
+
+    /**
+     * [Required] Create time [yyyyMMddHHmmssSSS]
+     */
+    private String            ctime            = TimeUtil.getCurrentDateTime("yyyyMMddHHmmssSSS");
+
+    /**
+     * The sign string, default value "SAMPLE"
+     */
+    private String            sign             = "SAMPLE";
+
+    /**
+     * [0 success, other error code]
+     */
+    private String            result           = "";
+
+    /**
+     * description
+     */
+    private String            msg              = "";
 
     public int getCode() {
         return this.code;
@@ -131,8 +144,7 @@ public class Header implements Serializable {
 
     @Override
     public String toString() {
-        return "Header [code=" + this.code + ", rpc=" + this.rpc + ", serial=" + this.serial + ", itype=" + this.itype
-                + ", add=" + this.add + ", ctime=" + this.ctime + ", sign=" + this.sign + ", result=" + this.result
-                + ", msg=" + this.msg + "]";
+        return "Header [code=" + this.code + ", rpc=" + this.rpc + ", serial=" + this.serial + ", itype=" + this.itype + ", add=" + this.add + ", ctime="
+                + this.ctime + ", sign=" + this.sign + ", result=" + this.result + ", msg=" + this.msg + "]";
     }
 }
