072b83dd54b792edb0bc37b19e8e724a93cd6c49
sebinson <sebinson@163.com>
2015-04-22 18:12:23 +0800

    xxx

---
+++ sebinson-sample-message/sebinson-sample-message-collection/src/main/java/net/sebinson/sample/message/collection/core/service/ProcessVersionControlService.java
@@ -0,0 +1,42 @@
+package net.sebinson.sample.message.collection.core.service;
+
+import net.sebinson.framework.message.transport.exception.TransportException;
+import net.sebinson.framework.message.transport.protocol.RemotingCommand;
+
+public interface ProcessVersionControlService
+{
+    /**
+     * 处理同步登陆请求
+     * @param request 对应请求报文
+     * @return 处理成功返回Object[0]=RemotingCommand返回给客户端的数据，object[1]=ClientInfoMsg
+     * 处理失败直接抛TransportException异常即可
+     * @throws TransportException 处理失败返回  
+     * TransportException.errorCode即为返回对方报文Header的resutl, TransportException.message即为返回对方报文Header的msg
+     */
+    public Object[] processLoginRequestSync(RemotingCommand request) throws TransportException;
+
+    /**
+     * 处理没有应答的请求
+     * @param request 对应请求报文
+     * @throws TransportException 处理失败返回  
+     * TransportException.errorCode即为返回对方报文Header的resutl, TransportException.message即为返回对方报文Header的msg
+     */
+    public void processRequestUnreply(RemotingCommand request) throws TransportException;
+
+    /**
+     * 处理同步应答的请求
+     * @param request 对应请求报文
+     * @return 处理成功返回对方的报文，处理失败直接抛TransportException异常即可
+     * @throws TransportException 处理失败返回  
+     * TransportException.errorCode即为返回对方报文Header的resutl, TransportException.message即为返回对方报文Header的msg
+     */
+    public RemotingCommand processRequestSync(RemotingCommand request) throws TransportException;
+
+    /**
+     * 处理异步应答的请求
+     * @param request 对应请求报文
+     * @throws TransportException 处理失败返回  
+     * TransportException.errorCode即为返回对方报文Header的resutl, TransportException.message即为返回对方报文Header的msg
+     */
+    public void processRequestAsync(RemotingCommand request) throws TransportException;
+}
--- sebinson-sample-message/sebinson-sample-message-collection/src/main/java/net/sebinson/sample/message/collection/core/service/message/transport/AbstractBusinessRequestProcessorService.java
+++ sebinson-sample-message/sebinson-sample-message-collection/src/main/java/net/sebinson/sample/message/collection/core/service/message/transport/AbstractBusinessRequestProcessorService.java
@@ -1,9 +1,11 @@
 package net.sebinson.sample.message.collection.core.service.message.transport;
 
+import net.sebinson.common.utils.SpringBeanUtil;
 import net.sebinson.framework.message.transport.exception.TransportException;
 import net.sebinson.framework.message.transport.processor.RequestProcessor;
 import net.sebinson.framework.message.transport.protocol.RemotingCommand;
 import net.sebinson.sample.message.collection.core.service.CollectionBaseService;
+import net.sebinson.sample.message.collection.core.service.ProcessVersionControlService;
 
 public abstract class AbstractBusinessRequestProcessorService implements RequestProcessor, CollectionBaseService {
 
@@ -13,21 +15,12 @@ public abstract class AbstractBusinessRequestProcessorService implements Request
         String itype = request.getHeader().getItype();
         ProcessVersionControlService requestProcessor = null;
         try {
-            requestProcessor = SpringBeanUtils.getBean(itype + "_" + version, ProcessVersionControlService.class);
+            requestProcessor = SpringBeanUtil.getBean(itype + "_" + version, ProcessVersionControlService.class);
         } catch (Exception e) {
-            throw new TransportException(TransportException.EORROR_TRANSPORT, "子类[" + this.getClass() + "]没有重写["
-                    + Thread.currentThread().getStackTrace()[1].getMethodName() + "]方法.业务逻辑没有实现.");
+            throw new TransportException(TransportException.EORROR_TRANSPORT, "");
         }
         requestProcessor.processRequestUnreply(request);
     }
-
-    /**
-     * 处理同步应答的请求
-     * 
-     * @param request 对应请求报文
-     * @return 处理成功返回对方的报文，处理失败直接抛TransportException异常即可
-     * @throws TransportException 处理失败返回 TransportException.errorCode即为返回对方报文Header的resutl, TransportException.message即为返回对方报文Header的msg
-     */
 
     @Override
     public RemotingCommand processRequestSync(RemotingCommand request) throws TransportException {
@@ -35,20 +28,12 @@ public abstract class AbstractBusinessRequestProcessorService implements Request
         String itype = request.getHeader().getItype();
         ProcessVersionControlService requestProcessor = null;
         try {
-            requestProcessor = SpringBeanUtils.getBean(itype + "_" + version, ProcessVersionControlService.class);
+            requestProcessor = SpringBeanUtil.getBean(itype + "_" + version, ProcessVersionControlService.class);
         } catch (Exception e) {
-            throw new TransportException(TransportException.EORROR_TRANSPORT, "子类[" + this.getClass() + "]没有重写["
-                    + Thread.currentThread().getStackTrace()[1].getMethodName() + "]方法.业务逻辑没有实现.");
+            throw new TransportException(TransportException.EORROR_TRANSPORT, "");
         }
         return requestProcessor.processRequestSync(request);
     }
-
-    /**
-     * 处理异步应答的请求
-     * 
-     * @param request 对应请求报文
-     * @throws TransportException 处理失败返回 TransportException.errorCode即为返回对方报文Header的resutl, TransportException.message即为返回对方报文Header的msg
-     */
 
     @Override
     public void processRequestAsync(RemotingCommand request) throws TransportException {
@@ -56,10 +41,9 @@ public abstract class AbstractBusinessRequestProcessorService implements Request
         String itype = request.getHeader().getItype();
         ProcessVersionControlService requestProcessor = null;
         try {
-            requestProcessor = SpringBeanUtils.getBean(itype + "_" + version, ProcessVersionControlService.class);
+            requestProcessor = SpringBeanUtil.getBean(itype + "_" + version, ProcessVersionControlService.class);
         } catch (Exception e) {
-            throw new TransportException(TransportException.EORROR_TRANSPORT, "子类[" + this.getClass() + "]没有重写["
-                    + Thread.currentThread().getStackTrace()[1].getMethodName() + "]方法.业务逻辑没有实现.");
+            throw new TransportException(TransportException.EORROR_TRANSPORT, "");
         }
         requestProcessor.processRequestAsync(request);
     }
